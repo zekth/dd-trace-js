b2c4b712cd8e84f153811e585e2583d98f75e2be
rochdev <roch.devost@datadoghq.com>
2021-05-17 20:49:37 -0400

    wip

--- packages/datadog-plugin-oracledb/test/index.spec.js
+++ packages/datadog-plugin-oracledb/test/index.spec.js
@@ -19,19 +19,24 @@ describe('Plugin', () => {
 
   describe('oracledb', () => {
     withVersions(plugin, 'oracledb', version => {
-      describe('without pool', () => {
-        describe('without configuration', () => {
-          before(async () => {
-            await agent.load('oracledb')
-            oracledb = require(`../../../versions/oracledb@${version}`).get()
-            connection = await oracledb.getConnection(config)
+      describe('without configuration', () => {
+        before(async () => {
+          await agent.load('oracledb')
+          oracledb = require(`../../../versions/oracledb@${version}`).get()
+        })
+        after(async () => {
+          await agent.close()
+        })
+
+        describe('with connection', () => {
+          before(() => {
+            connection = oracledb.getConnection(config)
           })
           after(async () => {
             await connection.close()
-            await agent.close()
           })
 
-          it.only('should be instrumented correctly with correct tags', done => {
+          it('should be instrumented for promise API', done => {
             agent.use(traces => {
               expect(traces[0][0]).to.have.property('name', 'oracle.query')
               expect(traces[0][0]).to.have.property('resource', dbQuery)
@@ -45,45 +50,8 @@ describe('Plugin', () => {
             }).then(done, done)
             connection.execute(dbQuery)
           })
-        })
-        describe('with configuration', () => {
-          before(async () => {
-            await agent.load('oracledb', { service: 'custom' })
-            oracledb = require(`../../../versions/oracledb@${version}`).get()
-            connection = await oracledb.getConnection(config)
-          })
-          after(async () => {
-            await connection.close()
-            await agent.close()
-          })
 
-          it('should be instrumented correctly with correct tags', done => {
-            agent.use(traces => {
-              expect(traces[0][0]).to.have.property('service', 'custom')
-            }).then(done, done)
-            connection.execute(dbQuery)
-          })
-        })
-        describe('without configuration with callback', () => {
-          before((done) => {
-            agent.load('oracledb').then(() => {
-              oracledb = require(`../../../versions/oracledb@${version}`).get()
-              oracledb.getConnection(config, (err, _connection) => {
-                if (err) {
-                  done(err)
-                  return
-                }
-                connection = _connection
-                done()
-              })
-            })
-          })
-          after(async () => {
-            await connection.close()
-            await agent.close()
-          })
-
-          it('should be instrumented correctly with correct tags', done => {
+          it('should be instrumented for callback API', done => {
             let callbackRan
             connection.execute(dbQuery, err => {
               expect(err).to.be.null
@@ -103,19 +71,15 @@ describe('Plugin', () => {
             }).then(done, done)
           })
         })
-      })
-      describe('with pool', () => {
-        describe('without configuration', () => {
+
+        describe('with pool', () => {
           before(async () => {
-            await agent.load('oracledb')
-            oracledb = require(`../../../versions/oracledb@${version}`).get()
             pool = await oracledb.createPool(config)
             connection = await pool.getConnection()
           })
           after(async () => {
             await connection.close()
             await pool.close()
-            await agent.close()
           })
 
           it('should be instrumented correctly with correct tags', done => {
