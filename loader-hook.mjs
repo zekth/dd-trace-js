d7a8b2dc243cb87eb7f694e9a11bc1296f71abe4
Bryan English <bryan.english@datadoghq.com>
2021-09-20 17:34:42 -0400

    filter out unneeded files for wrapping in iitm hook

--- loader-hook.mjs
+++ loader-hook.mjs
@@ -1,1 +1,39 @@
-export * from 'import-in-the-middle/hook.mjs'
+import { resolve as origResolve, getFormat, getSource } from 'import-in-the-middle/hook.mjs'
+import path from 'path'
+import names from './packages/dd-trace/src/plugins/all-shimmed-files.js'
+
+const nodeRe = /^node:(.*)/
+const fileRe = /^file:\/\//
+const notBareRe = /^[./]/
+const modulesRe = /.*node_modules\/(.*)/
+
+export async function resolve (specifier, context, parentResolve) {
+  if (context.parentURL && context.parentURL.startsWith('iitm:')) {
+    return parentResolve(specifier.replace('iitm:', ''), context, parentResolve)
+  }
+  let name = specifier
+  if (name.startsWith('iitm:')) return origResolve(specifier, context, parentResolve)
+  const matchedNode = name.match(nodeRe)
+  if (matchedNode) {
+    name = matchedNode[1]
+    return (names.has(name) ? origResolve : parentResolve)(specifier, context, parentResolve)
+  }
+  name = name.replace(fileRe, '')
+  if (!notBareRe.test(name)) {
+    // bare specifier. just search.
+    return (names.has(name) ? origResolve : parentResolve)(specifier, context, parentResolve)
+  }
+  if (!path.isAbsolute(name)) {
+    name = path.relative(context.parentURL, name)
+  }
+  const matchedFromModules = name.match(modulesRe)
+  if (!matchedFromModules) {
+    return parentResolve(specifier, context, parentResolve)
+  }
+  name = matchedFromModules[1]
+  return (names.has(name) ? origResolve : parentResolve)(specifier, context, parentResolve)
+}
+
+export { getFormat }
+
+export { getSource }
